dd31da247552012793c7bbd6562f451d4c199b48
tianhao <2247333869@qq.com>
2019-01-15 22:11:50 +0800

    BasicVersion

---
+++ pt/src/components/Login/login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="login">
+      <form action="login.vue">
+        <p>登录到您的账户</p>
+        <input type="text" v-model="username">
+        <input type="text" v-model="password">
+        <input type="submit">
+      </form>
+  </div>
+</template>
+
+<script>
+
+  export default{
+
+    data(){
+      return{
+      username:"",
+      password:"",
+      }
+    },
+    methods:{
+
+      login:function () {
+        this.axios.post('/user', {
+          firstName: 'Fred',
+          lastName: 'Flintstone'
+        })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
+
+    }
+  }
+
+</script>
+<style scoped>
+
+  @import "./../../../static/Style/login.css";
+
+</style>
